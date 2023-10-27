9b473d64571ecdbc9599f11ed394192bf892603e
Aparna <aparnageorge2401@gmail.com>
2023-10-27 15:44:49 +0100

    Clean up code

--- src/test/java/People/PersonTest.java
+++ src/test/java/People/PersonTest.java
@@ -1,6 +1,6 @@
 package People;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -10,23 +10,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonTest {
     Person test;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
         test = new Person("Test","Person",new GregorianCalendar(1999, Calendar.DECEMBER, 11));
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void findAgeOfPerson() {
         int expectedOutput = 23;
         int actualOutput = test.findAgeOfPerson();
         assertEquals(expectedOutput, actualOutput);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void millisToYears() {
         long millis = 31557600000L;
         int expectedOutput = 1;
@@ -42,14 +42,14 @@ class PersonTest {
         assertEquals(expectedOutput, actualOutput );
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void formatDate() {
         String expectedOutput = "11-Dec-1999";
         String actualOutput = test.formatDate(test.getDateOfBirth());
         assertEquals(expectedOutput,actualOutput);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testToString() {
         String expectedOutput = "Name: Test Person Age: 23 Date Of Birth: 11-Dec-1999";
         String actualOutput = test.toString();
